60ebf19c1df85198ce46cacfb9ec28a56b035d2d
mfahad <sheikhshaikhfahad@gmail.com>
2025-05-04 01:01:38 +0500

    basic tests

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -8,6 +8,8 @@ import {BasicNft} from "../src/BasicNft.sol";
 contract BasicNftTest is Test { 
     DeployBasicNft public deployer;
     BasicNft public basicNft;
+    address public USER = makeAddr("user");
+    string public constant SHIBA = "ipfs://QmVQ9FRoKdpQLPdD79ByaY3sUQEV1pHq2ek287H5FLSbqG";
 
     function setUp() public {
         deployer = new DeployBasicNft();
@@ -18,5 +20,13 @@ contract BasicNftTest is Test {
     string memory expectedName = "Dogie";
     string memory actualName = basicNft.name();
     assert(keccak256(abi.encodePacked(expectedName)) == keccak256(abi.encodePacked(actualName)));
+    }
+
+    function testCanMintAndHaveBalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(SHIBA);
+        
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(keccak256(abi.encodePacked(basicNft.tokenURI(0))) == keccak256(abi.encodePacked(SHIBA)));
     }
 }
