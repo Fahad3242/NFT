d5e10a35830955429127baf8a0160e0e15047a72
mfahad <sheikhshaikhfahad@gmail.com>
2025-05-04 00:37:45 +0500

    Started writing tests

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -9,5 +9,14 @@ contract BasicNftTest is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
 
-    function setUp() public {}
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+    string memory expectedName = "Dogie";
+    string memory actualName = basicNft.name();
+    assert(keccak256(abi.encodePacked(expectedName)) == keccak256(abi.encodePacked(actualName)));
+    }
 }
