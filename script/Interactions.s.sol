9dc4d03a6050a37561eeff3f51f5bbdbcf4ac6b0
mfahad <sheikhshaikhfahad@gmail.com>
2025-05-09 21:56:31 +0500

    interactions script

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -2,8 +2,24 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 
 
 contract MintBasicNft is Script{
-    function run() external {}
+
+    string public constant TOKENURI = "ipfs://QmVQ9FRoKdpQLPdD79ByaY3sUQEV1pHq2ek287H5FLSbqG";
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNft", block.chainid);
+
+        mintNftOnContract(mostRecentlyDeployed);
+
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNft(contractAddress).mintNft(TOKENURI);
+        vm.stopBroadcast();
+    }
 }
