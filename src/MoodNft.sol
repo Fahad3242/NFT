e93b8247af1b5494e0869ba0931af806d29713b8
mfahad <sheikhshaikhfahad@gmail.com>
2025-05-11 00:34:40 +0500

    MoodNFt

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -4,5 +4,23 @@ pragma solidity ^0.8.18;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MoodNft is ERC721 {
-    constructor(string memory sadSvg, string memory happySvg) ERC721("MoodNft", "MN") {}
+    string private s_sadSvg;
+    string private s_happySvg;
+    uint256 private s_tokenCounter;
+
+    constructor(string memory sadSvg, string memory happySvg) ERC721("MoodNft", "MN") {
+        s_sadSvg = sadSvg;
+        s_happySvg = happySvg;
+        s_tokenCounter = 0;
+
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    function tokenURI(uint256 tokenId) public view override returns (string memory){
+
+    }
 }
