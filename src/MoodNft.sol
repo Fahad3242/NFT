aaca48dbbb723536d87ed62ae92e74b2936c0d26
mfahad <sheikhshaikhfahad@gmail.com>
2025-05-21 00:02:50 +0500

    Flipping mood logic

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -61,9 +61,16 @@ contract MoodNft is ERC721 {
         }
 
     function flipMood(uint256 tokenId) public {
-    if(getApproved(tokenId) != msg.sender && ownerOf(tokenId) != msg.sender) {
+    if (getApproved(tokenId) != msg.sender && ownerOf(tokenId) != msg.sender) {
         revert MoodNFT__CantFlipMoodIfNotOwner();
+    }
+
+    if (s_tokenIdToMood[tokenId] == Mood.HAPPY){
+        s_tokenIdToMood[tokenId] = Mood.SAD;
+    } else {
+        s_tokenIdToMood[tokenId] = Mood.HAPPY;
+    }
     }
 }
 
-    }
+    
